efd1599d2df36cc17964746ada216ed5843c6546
CakeCrusher <1sebastian1sosa1@gmail.com>
2022-11-05 14:41:59 -0400

    linkedpush now allows you to quickly set up #github webhooks directly from your #dashboard , so no more getting bogged down setting up webhooks. Simply click 'Add to all', and all your current #repositories will be setup! @linkedpush

--- routes/user.js
+++ routes/user.js
@@ -5,12 +5,40 @@ const { rootUrl } = require("../utils/constants");
 const { verifyUser } = require("../utils/middleware");
 const ejs = require("ejs");
 
-router.get("/:id", verifyUser, (req, res) => {
+router.get("/:id", verifyUser, async (req, res) => {
   const deleteDataUrl = `${rootUrl(req)}/user/delete/${req.params.id}`;
   const webhookUrl = `${rootUrl(req)}/publish/${req.params.id}`;
+  // make an api request to retrieve the github user
+  const fetchUserInfo = await axios.get("https://api.github.com/user", {
+    headers: {
+      Authorization: `Bearer ${res.locals.user.githubToken}`,
+    },
+  });
+  const fetchRepos = await axios.get(
+    fetchUserInfo.data.repos_url + "?sort=pushed&per_page=50",
+    {
+      headers: {
+        Authorization: `Bearer ${res.locals.user.githubToken}`,
+      },
+    }
+  );
+
+  // reduce repos to only contain the following data: owner_name, repo_name,
+  const mappedRepos = fetchRepos.data.map((repo) => {
+    return {
+      repo_name: repo.name,
+      html_url: repo.html_url,
+    };
+  });
   ejs.renderFile(
     __dirname + "/../views/userDashboard.ejs",
-    { deleteDataUrl, webhookUrl },
+    {
+      deleteDataUrl,
+      webhookUrl: "https://example.com",
+      githubToken: res.locals.user.githubToken,
+      owner_name: fetchUserInfo.data.login,
+      repos: mappedRepos,
+    },
     (err, str) => {
       if (err) {
         console.log(err);
